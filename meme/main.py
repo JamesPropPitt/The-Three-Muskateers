e678864434f000af890c64b12882ffd70ea7fa2d
hannahlu <HU196882@falmouth.ac.uk>
2016-10-21 15:14:28 +0100

    Pycharm Meme Project

---
+++ meme/main.py
@@ -0,0 +1,6 @@
+import pygame, sys
+from pygame.locals import *
+
+#main page for meme project
+#Maybe have 'menu' here?
+#Create each meme in seperate file and then call to here? Might be too lengthy?
